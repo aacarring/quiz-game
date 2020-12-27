646c3ab782da0d82158b98b94241e472973f7885
Alexa Carrington <acarring@wellesley.edu>
2020-12-27 12:44:27 -0500

    Added but commented out more functions in js

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-const question = document.querySelector('.question');
+let question = document.querySelector('.question');
+const questions = [
+    "Who painted the Mona Lisa?",
+    "What nut is in the center of a Ferrero Rocher?",
+    "Which country is Brie cheese originally from?",
+    "What is the largest country in the world?",
+    "What is the capital of Iceland?"
+];
 
 
-function checkAnswer() {
-    let userInput = input.value;
-    if (userInput === "") return;
-    let regexp = /(leonardo)?(\s)da(\s)?vinci/i;
-    if (userInput.match(regexp)) {
-        alert("Correct!");
-        nextQuestion();
-    } else {
-        alert("Whoops, try again.");
+/* function checkAnswer() {
+    if (question.textContent === questions[0]) {
+        questionOne();
+    } else if (question.textContent === questions[1]) {
+        questionTwo();
+    } else if (question.textContent === questions[2]) {
+        questionsThree();
+    } else if (question.textContent === questions[3]) {
+        questionsFour();
+    } else if (question.textContent === questions[4]) {
+        questionFive();
     }
-    input.value = "";
 }
+
+    function questionOne() {
+        let userInput = input.value;
+        if (userInput === "") return;
+        let davinciRegex = /(leonardo)?(\s)?da(\s)?vinci/i;
+        if (userInput.match(davinciRegex)) {
+            alert("Correct! Four more to go.");
+            question.textContent = questions[1];
+        } else {
+            alert("Whoops, try again.");
+        }
+        input.value = "";
+    }
+
+    function questionTwo() {
+        let userInput = input.value;
+        let hazelnutRegex = /(a)?(\s)?hazelnut/i;
+        if (userInput.match(hazelnutRegex)) {
+            alert("Correct! Three more to victory.")
+        } else {
+            alert("Have you really never had a Ferrero Rocher? Try again.");
+        }
+        input.value = "";  
+    }
+*/
 
 button.addEventListener('click', checkAnswer);
