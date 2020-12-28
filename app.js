75d37ede7f18b4084e639d2ab0ba9931fab87092
Alexa Carrington <acarring@wellesley.edu>
2020-12-28 10:45:36 -0500

    Edited questions array into array of objects with questions and answers

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 let question = document.querySelector('.question');
-const questions = [
-    "Who painted the Mona Lisa?",
-    "What nut is in the center of a Ferrero Rocher?",
-    "Which country is Brie cheese originally from?",
-    "What is the largest country in the world?",
-    "What is the capital of Iceland?"
-];
+const allQuestions = [
+    {
+        question: "Who painted the Mona Lisa?",
+        answers: {
+            a: "da Vinci",
+            b: "Michelangelo"
+        },
+        question: "What nut is in the center of a Ferrero Rocher?",
+        answers: {
+            a: "Macadamia",
+            b: "Hazelnut"
+        },
+        question: "Which country is Brie cheese originally from?",
+        answers: {
+            a: "France",
+            b: "Belgium"
+        },
+        question: "What is the largest country in the world?",
+        answers: {
+            a: "India",
+            b: "Russia"
+        },
+        question: "What is the capital of Iceland?",
+        answers: {
+            a: "Amsterdam",
+            b: "Reykjavík"
+        },
+        
+    }];
 
-//presently, the problem is that each click runs checkAnswer, which only deals with the first question and will fail to allow the user to move on to further questions, must resolve
-/* function checkAnswer() {
-    question.textContent = questions[0];
-    let userInput = input.value;
-    let davinciRegex = /(leonardo)?(\s)?da(\s)?vinci/i;
-    if (userInput.match(davinciRegex)) {
-        alert("Correct!");
-        checkTwo();
-    } else {
-        alert("Whoops, try again...");
-    }
-    input.value = "";
-
-    function checkTwo() {
-        question = questions[1];
-        let hazelnutRegex = /(a)?(\s)?hazelnut/i;
-        if (userInput.match(hazelnutRegex)) {
-            alert("Correct!");
-        } else {
-            alert("Nope.");
-        }
-    }
-}
-*/
 
 
 button.addEventListener('click', checkAnswer);
